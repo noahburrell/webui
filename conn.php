e3b699cf04c4f4ad8e90e059dafc3f84cdc365bb
Noah Burrell <noah@burrelln.tech>
2018-07-17 15:21:15 -0400

    Update conn.php

--- conn.php
+++ conn.php
@@ -4,15 +4,15 @@ $mysql = array(
     "port" => "3306",
     "user" => "root",
     "password" => "password",
-    "db" => "telus",
+    "db" => "",
     "loginTable" => "loginInfo",
     "recoveryTable" => "passwordRecovery",
     "persistentLogin" => "auth_tokens"
 );
 
-$recaptcha_key = "6LdE3FoUAAAAAAZ1x-soMD6IdqUPOjlf5X-0xk4Z";
+$recaptcha_key = "";
 
 $mailjet = array(
-    "public" => "cc0e7c70bfc40c30d4fd3e16d4441d42",
-    "private" => "76b2263a5a6d72fb3b395406f8769280"
+    "public" => "",
+    "private" => ""
 );
