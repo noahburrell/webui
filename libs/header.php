34b113d66dc9bebe3c36938bbbff63922212f432
noah <noahburrell0@gmail.com>
2018-07-17 10:30:22 -0400

    Remove legacy code and update path to user images.

--- libs/header.php
+++ libs/header.php
@@ -6,18 +6,7 @@
         <td>
             <h1><?php echo $pageTitle; ?></h1></td>
         <td class="loggedIn">
-            <a href="<?php getURL("profilesettings.php"); ?>"><img src="<?php getURL($_SESSION['profile_img']); ?>" alt="Avatar"/></a><br/><?php echo $_SESSION["fname"]." ".$_SESSION['lname']; ?>
+            <a href="<?php getURL("profilesettings.php"); ?>"><img src="<?php getURL("/images/"+$_SESSION['profile_img']); ?>" alt="Avatar"/></a><br/><?php echo $_SESSION["fname"]." ".$_SESSION['lname']; ?>
         </td>
     </tr>
 </table>
-
-<!--
-<div class="grid-row header">
-  <div class="xs-6">
-    <h1><?php echo $pageTitle; ?></h1>
-  </div>
-  <div class="loggedIn xs-6">
-    <a href="<?php getURL("profilesettings.php"); ?>"><img src="<?php getURL($_SESSION['profile_img']); ?>" alt="Avatar"/></a><br/><?php echo $_SESSION["fname"]." ".$_SESSION['lname']; ?>
-  </div>
-</div>
--->
