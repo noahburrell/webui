ec3a51dd039c2729340700967b48d342d573cd77
noah <noahburrell0@gmail.com>
2018-07-26 17:26:10 -0400

    Remove unnecessary devices icon

--- libs/navbar.php
+++ libs/navbar.php
@@ -4,19 +4,19 @@
 
 <div class="grid-row">
     <div class="staticNavBar fixed-bottom">
-        <div class="xs-2 offset-xs-1 active">
+        <div class="xs-3">
             <a href="<?php getURL("networks.php"); ?>"><img class="mx-2 my-1" src="images/icons/networks.png" alt="Networks"/><br/>Networks</a>
         </div>
-        <div class="xs-2">
+        <!-- <div class="xs-2">
             <a href="<?php getURL("devices.php"); ?>"><img class="mx-2 my-1" src="images/icons/devices.png" alt="Devices"/><br/>Devices</a>
-        </div>
-        <div class="xs-2">
+        </div>-->
+        <div class="xs-3">
             <a href="<?php getURL("users.php"); ?>"><img  class="mx-2 my-1" src="images/icons/users.png" alt="Users"/><br/>Users</a>
         </div>
-        <div class="xs-2">
+        <div class="xs-3">
             <a href="<?php getURL("settings.php"); ?>"><img  class="mx-2 my-1" src="images/icons/settings.png" alt="Settings"/><br/>Settings</a>
         </div>
-        <div class="xs-2">
+        <div class="xs-3">
             <a href="<?php getURL("logout.php"); ?>"><img  class="mx-2 my-1" src="images/icons/logOut.png" alt="Log Out"/><br/>LogOut</a>
         </div>
     </div>
